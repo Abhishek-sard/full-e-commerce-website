a3d7ba20a18f1d65e5597dbb83ae09217c609393
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2023-12-02 22:20:44 +0545

    Add files via upload

---
+++ website.js
@@ -0,0 +1,12 @@
+
+let card = document.querySelectorAll(".trend");
+let mainPage = document.querySelectorAll(".main");
+let blog = document.querySelectorAll(".trends");
+function shops(){
+    Mainpage.style.display="none";
+
+
+}
+function blogs(){
+    
+}
